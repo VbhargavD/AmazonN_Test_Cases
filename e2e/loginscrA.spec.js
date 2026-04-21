0f6299eed546054c589174ff5a7745d9871f25a5
VbhargavD <bhargav.sgi@gmail.com>
2026-04-21 20:04:34 +0530

    Your descriptive commit message

--- e2e/loginscrA.spec.js
+++ e2e/loginscrA.spec.js
@@ -13,7 +13,7 @@ test.describe('verifyAmazonLogin', () => {
 
    )
 
-   test('amazon login', async ({ page }) => {
+   test.only('amazon login', async ({ page }) => {
       await page.goto("https://www.amazon.in/")
       await page.waitForTimeout(2000)
       
